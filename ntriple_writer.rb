d8b4ae7a24037d831c9c237e2b6a5d384357ec01
Robert Glaser <mail@robert-glaser.de>
2013-04-15 19:21:56 +0200

    unified thesauri as top terms

--- ntriple_writer.rb
+++ ntriple_writer.rb
@@ -5,15 +5,20 @@ require "bundler/setup"
 require "awesome_print"
 require "active_support/core_ext"
 require "csv"
+require "uuid"
 
 raise "You need to specify a CSV file" unless file = ARGV[0]
 raise "You need to specify a default namespace for ntriple generation" unless ns = ARGV[1]
+raise "You need to specify a title for a top term" unless top_term = ARGV[2]
 
 def skip?(line)
   line["Keyword Deutsch"].blank?
 end
 
 table = CSV.read(file, :col_sep => ";", :headers => true)
+
+top_term_id = top_term.parameterize
+tops = []
 
 table.each do |line|
   next if skip?(line)
@@ -44,7 +49,6 @@ table.each do |line|
   if broader
     puts "<#{ns}#{subject}> <http://www.w3.org/2004/02/skos/core#broader> <#{ns}_#{broader["Zeile"]}> ."
   else
-    puts "<#{ns}#{subject}> <http://www.w3.org/2004/02/skos/core#topConceptOf> <#{ns}scheme> ."
     narrowers = table.select { |l| l["Parent Zeile"].present? && l["Parent Zeile"] == line["Zeile"] }
     narrowers.each do |n|
       puts "<#{ns}#{subject}> <http://www.w3.org/2004/02/skos/core#narrower> <#{ns}_#{n["Zeile"]}> ."
@@ -54,4 +58,17 @@ table.each do |line|
   if translation = line["Keyword Übersetzung"]
     puts "<#{ns}#{subject}> <http://www.w3.org/2004/02/skos/core#altLabel> \"#{translation.strip}\"@de ."
   end
+
+  # top terms
+  if !line.has_key?("Parent Zeile") || (line.has_key?("Parent Zeile") && line["Parent Zeile"].blank?)
+    puts "<#{ns}#{subject}> <http://www.w3.org/2004/02/skos/core#broader> <#{ns}#{top_term_id}> ."
+    tops << subject
+  end
+end
+
+puts "<#{ns}#{top_term_id}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2004/02/skos/core#Concept> ."
+puts "<#{ns}#{top_term_id}> <http://www.w3.org/2004/02/skos/core#topConceptOf> <#{ns}scheme> ."
+puts "<#{ns}#{top_term_id}> <http://www.w3.org/2004/02/skos/core#prefLabel> \"#{top_term}\"@de ."
+tops.each do |top|
+  puts "<#{ns}#{top_term_id}> <http://www.w3.org/2004/02/skos/core#narrower> <#{ns}#{top}> ."
 end
